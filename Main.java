ebbd0b81a240910e2fbe07d00088389ddcbe89b2
Diego Costa <dicosta@gmail.com>
2019-01-20 18:31:16 -0300

    removed unused code

--- Main.java
+++ Main.java
@@ -14,35 +14,8 @@ public class Main {
     public static void main(final String[] args) {        
         if (args.length < 1 || args.length > 1) {
             out.print("param missing\n");
-        } else {
-            /*
-            File[] files = new File(args[0]).listFiles();
-
-            Arrays.sort(files, new Comparator<File>() {
-                @Override
-                public int compare(File o1, File o2) {
-                    return o1.getName().compareTo(o2.getName());
-                }
-            });
-
-            Calendar c = Calendar.getInstance();
-            c.set(2010, Calendar.MARCH, 20);
-            for (File file : files) {
-                c.add(Calendar.DATE, 1); 
-                out.println("directory: " + file.getName());                
-                Path p = Paths.get(file.getPath());
-                try {
-                    Files.setAttribute(p, "lastModifiedTime", FileTime.fromMillis(c.getTimeInMillis()));
-                    Files.setAttribute(p, "creationTime", FileTime.fromMillis(c.getTimeInMillis()));
-                    Files.setAttribute(p, "lastAccessTime", FileTime.fromMillis(c.getTimeInMillis()));
-                } catch (IOException e) {
-                    out.print("cannot write attribute\n");
-                }
-            }
-            */
+        } else {            
             sortInsideDirectory(args[0]);
-
-            //out.println("found: " + files.length);
         }
     }
 
